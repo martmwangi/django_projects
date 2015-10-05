e50ced27f0b762ced0079b3e9e75e7f4858ede22
martmwangi <luthermwangi2@gmail.com>
2015-10-05 12:04:55 +0300

    Added form validation

--- home/forms.py
+++ home/forms.py
@@ -4,5 +4,12 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['full_name','email']
+#        fields = ['full_name','email','age']
+        exclude = ['last_update']
         model = Student
+    def clean_age(self):
+        age = self.cleaned_data.get('get')
+        if age >120
+            raise forms.ValidationError("You may be too old for this class")
+
+        return age
