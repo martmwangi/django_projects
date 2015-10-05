ebc45ffd57f0fc4be4c7d7fca3a5a6b17855146b
martmwangi <luthermwangi2@gmail.com>
2015-10-05 11:35:15 +0300

    Added new file for form.py for the client side

--- home/models.py
+++ home/models.py
@@ -11,3 +11,14 @@ class Student(models.Model):
     
     def __str__(self):
         return self.full_name
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=200)
+    floor = models.CharField(max_length=100)
+    has_podium = models.CharField(max_length=100)
+    commissioned_date = models.DateField(auto_now=False, auto_now_add = True)
+    created_at = models.DateField(auto_now=False, auto_now_add = True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
