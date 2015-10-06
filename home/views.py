4f0df97b3c8d16c21bfea02d8de7e2f0042a41f3
martmwangi <luthermwangi2@gmail.com>
2015-10-06 10:28:37 +0300

    Added save function

--- home/views.py
+++ home/views.py
@@ -4,11 +4,20 @@ from .forms import StudentForm
 
 # Create your views here.
 def index(request):
-    form = StudentForm()
+    form = StudentForm(request.POST or None)
     context = {
-        "hello_message":"Hello Moringa",
+        "hello_message":"Register new Student",
         "form": form
 
     }
+
+    if form.is_valid():
+        form.save()
+        context = {
+            "hello_message": "Student Saved"
+
+        }
+    print (request.POST)
+
     return render(request,'index.html', context)
     return HttpResponse("Success")
